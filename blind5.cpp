4f46142b9ef9178dda5d4db1a8beedf1abb8c80a
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2025-01-14 22:58:19 +0530

    Daily update: 14-01-2025

--- blind5.cpp
+++ blind5.cpp
@@ -31,12 +31,39 @@ void generateRandomNumbers(int n) {
     cout << "\n";
 }
 
+string getRandomTopic(const vector<string>& topics) {
+    srand(time(0)); // Initialize random seed
+    int randomIndex = rand() % topics.size(); // Generate random index
+    return topics[randomIndex]; // Return the randomly selected topic
+}
+
+
 int main() {
     int n;
     cout << "Enter total questions solved: ";
     cin >> n;
 
+    vector<string> topics = {
+        "Array", "String", "Hash Table", "Dynamic Programming", "Math",
+        "Sorting", "Greedy", "Depth-First Search", "Binary Search", "Matrix",
+        "Tree", "Breadth-First Search", "Bit Manipulation", "Two Pointers",
+        "Prefix Sum", "Heap (Priority Queue)", "Binary Tree", "Stack", "Graph",
+        "Counting", "Sliding Window", "Backtracking", "Enumeration", "Union Find",
+        "Linked List", "Ordered Set", "Number Theory", "Monotonic Stack",
+        "Segment Tree", "Trie", "Bitmask", "Divide and Conquer", "Combinatorics",
+        "Queue", "Recursion", "Binary Indexed Tree", "Geometry",
+        "Binary Search Tree", "Hash Function", "Memoization", "String Matching",
+        "Topological Sort", "Shortest Path", "Rolling Hash", "Game Theory",
+        "Data Stream", "Monotonic Queue", "Brainteaser", "Randomized",
+        "Merge Sort", "Doubly-Linked List", "Counting Sort", "Iterator",
+        "Quickselect", "Suffix Array", "Bucket Sort", "Minimum Spanning Tree",
+        "Strongly Connected Component", "Radix Sort", "Biconnected Component"
+    };
+
+
     generateRandomNumbers(n);
+
+    cout << "Today's topic is : " << getRandomTopic(topics) << endl;
 
     return 0;
 }
