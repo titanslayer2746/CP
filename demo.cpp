828555649a62ba0a0493087ed04a25f78620cbe6
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2025-01-22 23:33:18 +0530

    Daily update: 22-01-2025

--- demo.cpp
+++ demo.cpp
@@ -8,25 +8,37 @@ int main() {
     int t;
     cin >> t;
     while(t--) {
-        int n;
-        cin>>n;
+        ll n,l,r;
+        cin>>n>>l>>r;
+        l--; r--;
 
-        vector<string> g(n);
-        for(int i=0; i<n; i++){
-            cin>>g[i];
+        vector<int> nums;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+
+            nums.push_back(a);
         }
 
-        vector<int> perm(n);
-        iota(perm.begin(),perm.end(),0);
-        sort(perm.begin(),perm.end(), [&](int u,int v){
-            if(u<v) return g[u][v]=='1';
-            return g[u][v]=='0';
-        });
-
-        for(int i=0; i<n; i++){
-            cout<<perm[i]+1<<" ";
+        ll ans1=0;
+        for(int i=l;i<=r;i++){
+            ans1 += nums[i];
         }
-        cout<<"\n";
+
+        ll ans2 = ans1, ans3 = ans1;
+        int temp = r-l+1;
+
+        sort(nums.begin(),nums.begin()+l-1);
+        sort(nums.begin()+r+1,nums.end());
+        sort(nums.begin()+l,nums.begin()+r);
+
+        ll extra1 = accumulate(nums.begin()+l,nums)
+
+        for(int i=0;i<min(l,temp);i++){
+            ans2 -= nums[i+temp];
+            ans2 += nums[i];
+        }
+
     }
     return 0;
 }
